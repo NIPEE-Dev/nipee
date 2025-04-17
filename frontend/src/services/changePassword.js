b4d6aa7aa3f418ea2d1985db84a9703c1d0e6909
lukaafff <luizagarcia2405@gmail.com>
2025-04-17 19:15:57 -0300

    enviar e-mail ao trocar senha

--- frontend/src/services/changePassword.js
+++ frontend/src/services/changePassword.js
@@ -1,15 +1,25 @@
 import api from '../apiex';
 
-export const changePassword = async (email, newPassword, newPasswordConfirmation) => {
+export const changePassword = async (email, code, newPassword, newPasswordConfirmation) => {
   try {
     const response = await api.post('/change-password', {
       email,
-      password: newPassword, 
-      password_confirmation: newPasswordConfirmation, 
+      code, 
+      password: newPassword,
+      password_confirmation: newPasswordConfirmation,
     });
 
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || 'Erro ao trocar a senha.');
+  }
+};
+
+export const sendVerificationCode = async (email) => {
+  try {
+    const response = await api.post('/send-verification-code', { email });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || 'Erro ao enviar o código de verificação.');
   }
 };
