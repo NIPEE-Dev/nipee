d7122ce4501eb0323bcfd7543f51e921dbb12980
lukaafff <luizagarcia2405@gmail.com>
2025-05-23 11:27:50 -0300

    voltando o navitem

--- frontend/src/components/SidebarWithHeader/NavItem.jsx
+++ frontend/src/components/SidebarWithHeader/NavItem.jsx
@@ -1,72 +1,37 @@
 import React from 'react';
 import { Flex, Icon } from '@chakra-ui/react';
-import { NavLink } from 'react-router-dom';
+import { NavLink as RouterDomLink } from 'react-router-dom';
 
-const NavItem = ({ icon, children, to, ...rest }) => {
-  return to ? (
-    <NavLink to={to} end>
-      {({ isActive }) => (
-        <Flex
-          align='center'
-          px='4'
-          pl='4'
-          py='3'
-          cursor='pointer'
-          color='inherit'
-          bg={isActive ? 'gray.200' : 'transparent'}
-          _hover={{
-            bg: 'purple.500',
-            color: 'white',
-          }}
-          role='group'
-          fontWeight='semibold'
-          transition='.15s ease'
-          {...rest}
-        >
-          {icon && (
-            <Icon
-              mx='2'
-              boxSize='4'
-              as={icon}
-              _groupHover={{
-                color: 'white',
-              }}
-            />
-          )}
-          {children}
-        </Flex>
-      )}
-    </NavLink>
-  ) : (
-    <Flex
-      align='center'
-      px='4'
-      pl='4'
-      py='3'
-      cursor='pointer'
-      color='inherit'
-      _hover={{
-        bg: 'purple.500',
-        color: 'white',
-      }}
-      role='group'
-      fontWeight='semibold'
-      transition='.15s ease'
-      {...rest}
-    >
-      {icon && (
-        <Icon
-          mx='2'
-          boxSize='4'
-          as={icon}
-          _groupHover={{
-            color: 'white',
-          }}
-        />
-      )}
-      {children}
-    </Flex>
-  );
-};
+const NavItem = ({ icon, children, color, ...rest }) => (
+  <Flex
+    align='center'
+    px='4'
+    pl='4'
+    py='3'
+    cursor='pointer'
+    color='inherit'
+    _hover={{
+      bg: 'purple.500',
+      color: 'white',
+    }}
+    role='group'
+    fontWeight='semibold'
+    transition='.15s ease'
+    as={rest.to ? RouterDomLink : 'span'}
+    {...rest}
+  >
+    {icon && (
+      <Icon
+        mx='2'
+        boxSize='4'
+        _groupHover={{
+          color: 'white',
+        }}
+        as={icon}
+      />
+    )}
+    {children}
+  </Flex>
+);
 
 export default NavItem;
