18851d14ba18926565b074d454c653800e363eac
lukaafff <luizagarcia2405@gmail.com>
2025-05-26 17:56:33 -0300

    feat: página da escola para ver atividades aprovadas pela empresaa

--- frontend/src/pages/ReportsFCT/ReportsSchool.jsx
+++ frontend/src/pages/ReportsFCT/ReportsSchool.jsx
@@ -1,8 +1,76 @@
+import React from 'react';
+import ResourceScreen from '../../components/ResourceScreen/ResourceScreen';
+import { CandidatesForm as Form } from '../../forms/CandidatesForm/CandidatesForm';
+import { Link } from '@chakra-ui/react';
+import routes from '../../routes';
+
 const ReportsSchool = () => {
-  return (
-    <div>
-       escola
-    </div>
+
+  const title = 'Atividades dos Candidatos';
+
+  return (  
+    <ResourceScreen
+      title={title}
+      permissions={['']}
+      resource='Candidates'
+      routeBase={routes} 
+      Form={Form}
+      canAdd={false}
+      canRemove={false}
+      canEdit={false}
+      resourceListProps={{
+        downloadButtonEnabled: false,
+      }}
+      filters={[
+        { field: 'name', header: 'Nome' },
+        {
+          field: 'status',
+          header: 'Status',
+          type: 'select',
+          options: [
+              { value: 'approved', header: 'Aprovadas' },
+          ]
+        },
+      ]}
+      columns={[
+        {
+          Header: 'Nome Candidato',
+          accessor: 'name'
+        },
+        {
+          Header: 'Empresa',
+          accessor: 'company'
+        },
+        {
+          Header: 'Título da Atividade',
+          accessor: 'activity'
+        },
+        {
+          Header: 'Data da Atividade',
+          accessor: 'date'
+        },
+        {
+          Header: 'Horas',
+          accessor: 'hours'
+        },
+        {
+          Header: 'Status',
+          accessor: 'status'
+        },
+        {
+          Header: 'Atividade',
+          accessor: (originalRow) => {
+            const filePath = `${import.meta.env.VITE_BACKEND_BASE_URL_EX}/storage/${originalRow.resume}`;
+            
+            return (
+              <Link target='_blank' href={filePath}>
+                Currículo de {originalRow.name} 
+              </Link>
+            );
+          }
+        },
+      ]}
+    />
   );
 };
 
