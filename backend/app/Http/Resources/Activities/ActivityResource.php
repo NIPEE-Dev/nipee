f514492308c467daa0af0e4164c7b69e63245bc5
Victor Murilo <victor2706.vm@gmail.com>
2025-05-27 14:22:09 -0300

    feat: resource de atividades

---
+++ backend/app/Http/Resources/Activities/ActivityResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Activities;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActivityResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "estimatedTime" => $this->estimated_time,
+            "activityDate" => $this->activity_date,
+            "description" => $this->description,
+            "status" => $this->status
+        ];
+    }
+}
