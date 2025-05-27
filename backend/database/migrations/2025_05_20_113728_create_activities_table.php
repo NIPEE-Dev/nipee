9c80dc91afb48637e0c59ad32e79a67a8f875d46
Victor Murilo <victor2706.vm@gmail.com>
2025-05-27 14:21:47 -0300

    feat: migration criando tabela de atividades

---
+++ backend/database/migrations/2025_05_20_113728_create_activities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Users\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return voidz
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->string('description');
+            $table->unsignedInteger('estimated_time');
+            $table->unsignedTinyInteger('status');
+            $table->date('activity_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+};
