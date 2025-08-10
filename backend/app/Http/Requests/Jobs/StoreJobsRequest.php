5c65d41d3e2f0a53a33c27ea0d6afc5ab380c3ac
Victor Murilo <victor2706.vm@gmail.com>
2025-08-10 14:22:36 -0300

    refactor: tirando obrigatoriedade de dois campos

--- backend/app/Http/Requests/Jobs/StoreJobsRequest.php
+++ backend/app/Http/Requests/Jobs/StoreJobsRequest.php
@@ -30,8 +30,8 @@ class StoreJobsRequest extends FormRequest
             'transport_voucher_value' => 'exclude_if:transport_voucher,0',
             'transport_voucher_nominal_value' => ['exclude_if:transport_voucher,0', 'string', 'max:255'],
 
-            'scholarship_value' => 'required',
-            'scholarship_nominal_value' => ['required', 'string', 'max:255'],
+            'scholarship_value' => 'string',
+            'scholarship_nominal_value' => ['string', 'max:255'],
             'available' => 'required|numeric',
             'type' => 'required|in:ES,EF',
             'fct_hours' => ['required_if:type,EF', 'integer'],
