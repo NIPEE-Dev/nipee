8b3ff49db02a104d5ee00a64902e45d3aa12018a
lukaafff <luizagarcia2405@gmail.com>
2025-09-03 08:59:45 -0300

    feat: resource para trazer dados de candidatos compativeis

--- backend/app/Http/Resources/Jobs/JobResource.php
+++ backend/app/Http/Resources/Jobs/JobResource.php
@@ -8,6 +8,7 @@ use App\Enums\GenderEnum;
 use App\Enums\PeriodEnum;
 use App\Enums\RolesEnum;
 use App\Http\Resources\Companies\CompanyResource;
+use App\Http\Resources\CompatibleCandidateResource;
 use App\Http\Resources\JobCandidateResource;
 use App\Models\Candidate;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -71,7 +72,7 @@ class JobResource extends JsonResource
             'status' => $this->status,
             'already_applied' => $this->when($roleId === RolesEnum::CANDIDATE->value, $this->candidates->where('id', $user->candidate->id ?? null)->first() !== null),
             'candidates' => $this->when($roleId === RolesEnum::COMPANY->value, JobCandidateResource::collection($this->candidates)),
-            'compatible_candidates' => $this->when($roleId === RolesEnum::COMPANY->value && isset($this->courses), $compatibleCandidates),
+            'compatible_candidates' => $this->when($roleId === RolesEnum::COMPANY->value && isset($this->courses), CompatibleCandidateResource::collection($compatibleCandidates)),
         ];
     }
 }
