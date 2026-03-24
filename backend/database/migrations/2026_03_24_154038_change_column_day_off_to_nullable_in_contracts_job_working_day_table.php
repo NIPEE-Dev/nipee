306599f955a9303e7a97717e55656146f4fe993f
Victor Murilo <victor2706.vm@gmail.com>
2026-03-24 15:41:33 -0300

    fix: arrumando erro do day_off na tabela contracts_job_working_day

---
+++ backend/database/migrations/2026_03_24_154038_change_column_day_off_to_nullable_in_contracts_job_working_day_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contracts_job_working_day', function (Blueprint $table) {
+            $table->string('day_off')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contracts_job_working_day', function (Blueprint $table) {
+            $table->string('day_off')->change();
+        });
+    }
+};
